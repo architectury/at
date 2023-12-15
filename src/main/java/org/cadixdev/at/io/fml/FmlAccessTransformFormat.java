caa2ed51c272492397c7669171ff5de98010e874
Juuz <6596629+Juuxel@users.noreply.github.com>
2023-12-15 15:39:50 +0200

    Update license years

--- src/main/java/org/cadixdev/at/io/fml/FmlAccessTransformFormat.java
+++ src/main/java/org/cadixdev/at/io/fml/FmlAccessTransformFormat.java
@@ -1,6 +1,7 @@
 /*
  * The MIT License (MIT)
  *
+ * Copyright (c) 2023 Architectury
  * Copyright (c) 2018 Minecrell (https://github.com/Minecrell)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
