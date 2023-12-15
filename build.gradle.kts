28d0610b288952533bde8cd1ac7fcc794b91e271
Juuz <6596629+Juuxel@users.noreply.github.com>
2023-12-15 15:33:23 +0200

    Update Gradle, remove old publishing details, start Architectury setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,18 @@
 plugins {
     `java-library`
-    signing
     `maven-publish`
-    id("net.minecrell.licenser") version "0.4.1"
+    id("net.kyori.indra.licenser.spotless") version "3.1.3"
 }
+
+val baseVersion: String by project
+version = baseVersion + "." + (System.getenv("GITHUB_RUN_NUMBER") ?: "9999")
+
+logger.lifecycle(":building at v${version}")
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
+    withSourcesJar()
+    withJavadocJar()
 }
 
 repositories {
@@ -25,93 +31,15 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-val sourceJar = task<Jar>("sourceJar") {
-    classifier = "sources"
-    from(sourceSets["main"].allSource)
+indraSpotlessLicenser {
+    licenseHeaderFile("LICENSE")
+    newLine(true)
 }
-
-val javadocJar = task<Jar>("javadocJar") {
-    classifier = "javadoc"
-    from(tasks["javadoc"])
-}
-
-artifacts {
-    add("archives", sourceJar)
-    add("archives", javadocJar)
-}
-
-val isSnapshot = version.toString().endsWith("-SNAPSHOT")
 
 publishing {
     publications {
         register<MavenPublication>("mavenJava") {
             from(components["java"])
-
-            artifact(sourceJar)
-            artifact(javadocJar)
-
-            pom {
-                val name: String by project
-                val description: String by project
-                val url: String by project
-                name(name)
-                description(description)
-                url(url)
-
-                scm {
-                    url(url)
-                    connection("scm:git:$url.git")
-                    developerConnection.set(connection)
-                }
-
-                issueManagement {
-                    system("GitHub Issues")
-                    url("$url/issues")
-                }
-
-                licenses {
-                    license {
-                        name("MIT License")
-                        url("https://opensource.org/licenses/MIT")
-                        distribution("repo")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id("jamierocks")
-                        name("Jamie Mansfield")
-                        email("jmansfield@cadixdev.org")
-                        url("https://www.jamiemansfield.me/")
-                        timezone("Europe/London")
-                    }
-                }
-            }
-        }
-    }
-
-    repositories {
-        val sonatypeUsername: String? by project
-        val sonatypePassword: String? by project
-        if (sonatypeUsername != null && sonatypePassword != null) {
-            val url = if (isSnapshot) "https://oss.sonatype.org/content/repositories/snapshots/"
-                else "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-            maven(url) {
-                credentials {
-                    username = sonatypeUsername
-                    password = sonatypePassword
-                }
-            }
         }
     }
 }
-
-signing {
-    sign(publishing.publications["mavenJava"])
-}
-
-tasks.withType<Sign> {
-    onlyIf { !isSnapshot }
-}
-
-operator fun Property<String>.invoke(v: String) = set(v)
